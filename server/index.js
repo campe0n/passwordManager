f974ea8c74bc1f49c034c10cd2933970ffa57d0a
melvin <melvinrf@Outlook.com>
2021-08-15 14:12:41 -0500

    added index server code and added install and develop scripts to server package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/atuh");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log('Server running...')
-})
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
