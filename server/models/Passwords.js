059dbb51e1f704e6793d227e5859b56ae51a9141
melvin <melvinrf@Outlook.com>
2021-08-16 11:51:40 -0500

    added passwords model

---
+++ server/models/Passwords.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const passwordsSchema = new Schema({
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  website: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const Passwords = mongoose.model("Passwords", passwordsSchema);
+
+module.exports = Passwords;
