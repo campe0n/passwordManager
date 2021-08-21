3b42a7e5bac2b7d6d0e88210b2577598547cf974
melvin <melvinrf@Outlook.com>
2021-08-21 12:35:55 -0500

    Fixed importing issue on client server

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Box from  '@material-ui/core/Box';
-import Link from '@material-ui/core/link';
-
+import React from "react";
+import { useLocation, useHistory, Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
 const footer = {
-  backgroundColor: '#3F51B5',
-  borderRadius: "10px 10px 10px 10px"
-}
-
-
+  backgroundColor: "#3F51B5",
+  borderRadius: "10px 10px 10px 10px",
+};
 
 export default function Footer() {
-    return <footer style={footer}>
+  return (
+    <footer style={footer}>
       <Box mt={8}>
-      <Typography variant="body2" color="textSecondary" align="center" >
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Amigos
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-    </Box>
-    </footer>;
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Copyright © "}
+          <Link color="inherit" href="https://material-ui.com/">
+            Amigos
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Box>
+    </footer>
+  );
 }
