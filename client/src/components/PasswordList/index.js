9dba2603a0c497c03e494ab4cd3d53788dbe42ae
melvin <melvinrf@Outlook.com>
2021-08-22 20:41:58 -0500

    added passwordlist component

---
+++ client/src/components/PasswordList/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const PasswordList = ({ passwords }) => {
+  if (!passwords.length) {
+    return <h3>No passwords yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{passwords.website}</h3>
+      <div>
+        {passwords &&
+          passwords.map((password) => (
+            <div key={password._id}>
+              <h4>{password.website}</h4>
+              <Link to={`passwords/${password._id}`}>View password.</Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default PasswordList;
