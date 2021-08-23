8aa11a95bee1ecd20f7d7d2f3efcf796b5552688
melvin <melvinrf@Outlook.com>
2021-08-23 11:08:39 -0500

    created styles object for passwordList component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,28 +7,6 @@ import { QUERY_PASSWORDS } from "../utils/queries";
 import PasswordList from "../components/PasswordList";
 
 const styles = {
-  divCard: {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-  },
-  card: {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "#3F51B5",
-    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-    transition: "0.3s",
-    width: "20%",
-    borderRadius: "10px 10px 10px 10px",
-    margin: "20px",
-  },
-  container: {
-    padding: "1px 1px 1px 2px",
-  },
-  noPassword: {
-    display: "flex",
-    justifyContent: "space-evenly",
-  },
   btn: {
     backgroundColor: "#03fcad",
     margin: "1rem",
@@ -56,16 +34,8 @@ const Dashboard = () => {
             Create Password
           </Link>
         </button>
-
-        <div>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <PasswordList passwords={passwords} />
-          )}
-        </div>
       </div>
-      );
+
       <div>
         {loading ? (
           <div>Loading...</div>
