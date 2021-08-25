e9631245d7ed933810fc3b07b88fb90f820098fc
melvin <melvinrf@Outlook.com>
2021-08-24 20:08:36 -0500

    added route to index.html

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/public/index.html")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
